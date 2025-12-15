79f1ee7372ce18f297d32b04c93a8695a063aa63
Joel Dare <joel@joeldare.com>
2025-12-15 06:08:57 -0700

    Add an index file and copy it into place

--- buoy.go
+++ buoy.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 )
+
+//go:embed assets/index.html
+var defaultIndex []byte
 
 func main() {
 	port := getServerPort()
-	fileServer := http.FileServer(http.Dir("./www"))
+	wwwDir, err := getWWWDir()
+	if err != nil {
+		fmt.Printf("Could not determine www directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := ensureIndexFile(wwwDir); err != nil {
+		fmt.Printf("Could not initialize index.html: %v\n", err)
+		os.Exit(1)
+	}
+
+	fileServer := http.FileServer(http.Dir(wwwDir))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/search", searchHandler)
-	fmt.Printf("Server started on port %s\n", port)
+	fmt.Printf("Serving %s on port %s\n", wwwDir, port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Printf(err.Error())
 	}
@@ -29,4 +45,33 @@ func getServerPort() string {
 		port = "8080"
 	}
 	return port
+}
+
+func getWWWDir() (string, error) {
+	dataHome := os.Getenv("XDG_DATA_HOME")
+	if dataHome == "" {
+		userHome, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+		dataHome = filepath.Join(userHome, ".local", "share")
+	}
+
+	wwwDir := filepath.Join(dataHome, "buoy", "www")
+	if err := os.MkdirAll(wwwDir, 0o755); err != nil {
+		return "", err
+	}
+
+	return wwwDir, nil
+}
+
+func ensureIndexFile(wwwDir string) error {
+	indexPath := filepath.Join(wwwDir, "index.html")
+	if _, err := os.Stat(indexPath); err == nil {
+		return nil
+	} else if !os.IsNotExist(err) {
+		return err
+	}
+
+	return os.WriteFile(indexPath, defaultIndex, 0o644)
 }
